018ccba324c7608fa6abe5c48162fe74926a5795
erramuzpe <asier.erramuzpe@gmail.com>
2017-07-18 09:55:54 +0200

    corrected script

--- run_all.py
+++ run_all.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python
 
-import sys
+# import sys
 import os
 
 try:
-	os.mkdir(".job/")
+    os.mkdir(".job/")
 except OSError:
-	pass
+    pass
 
 
-for i in range(20,2515):
-	filey = ".job/%s.job" %i
-	filey = open(filey,"w")
-	filey.writelines("#!/bin/bash\n")
-	filey.writelines("#SBATCH --job-name=%s\n" %i)
-	filey.writelines("#SBATCH --output=.out/%s.out\n" %i)
-	filey.writelines("#SBATCH --error=.out/%s.err\n" %i)
-	filey.writelines("#SBATCH -p large\n")
-	filey.writelines("#SBATCH --mem=10000\n")
-	filey.writelines("\n")
-	filey.writelines("module load Octave/4.0.0-foss-2015a\n")
-	filey.writelines("octave --no-gui --eval 'generate_mod(%s)'; exit;\n" %i)
-	filey.writelines("module unload Octave/4.0.0-foss-2015a\n")
-	filey.close()
-	os.system("sbatch  " + ".job/%s.job" %i)
+for subject_num in range(1, 2515):  # 1 -> n-1
+    i = str(subject_num).zfill(3)
+    filey = "job/sub-%s.job" % i
+    filey = open(filey, "w")
+    filey.writelines("#!/bin/bash\n")
+    filey.writelines("#SBATCH --job-name=sub-%s\n" % i)
+    filey.writelines("#SBATCH --output=.out/sub-%s.out\n" % i)
+    filey.writelines("#SBATCH --error=.out/sub-%s.err\n" % i)
+    filey.writelines("#SBATCH -p large\n")
+    filey.writelines("#SBATCH -n 4\n")
+    filey.writelines("#SBATCH -N 4\n")
+    filey.writelines("#SBATCH --mem=10000\n")
+    filey.writelines("\n")
 
+    filey.writelines("module load FreeSurfer/6.0.0-centos6_x86_64\n")
+    filey.writelines("recon-all -subjid sub-%s \
+                     -i file%s.nii \
+                     -all \
+                     -openmp 4 \
+                     \n" % (i, i))
+    filey.writelines("module unload FreeSurfer/6.0.0-centos6_x86_64\n")
+
+    filey.close()
+    os.system("sbatch  " + ".job/%s.job" % i)
