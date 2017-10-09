ff727332ae4111e1b40224550dbf94448da23e70
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-09 12:10:54 +0200

    rename bids script

---
+++ rename_bids.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 12:10:02 2017
+
+@author: asier
+"""
+
+import os
+from os.path import join as opj
+import shutil
+cwd = os.getcwd()
+
+data_path = opj(cwd, 'data')
+
+
+for file in os.listdir(data_path):
+    
+    if file.endswith('.nii'):
+        subject = file[:-8]
+        
+        os.makedirs(opj(data_path, 'raw', 'bids', subject, 'ses-001', 'anat'))
+        shutil.move(opj(data_path, file), 
+                    opj(data_path, 'raw', 'bids', subject, 'ses-001', 'anat'))
+
+dwi_data_path = opj(cwd, 'dwi')
+
+for subject in os.listdir(dwi_data_path):
+    print(subject)
+    try:
+        os.makedirs(opj(data_path, 'raw', 'bids', subject, 'ses-001', 'dwi'))
+    except:
+        pass
+    
+    shutil.move(opj(dwi_data_path, subject, 'dwi', subject+'_dwi.bval'),
+                opj(data_path, 'raw', 'bids', subject, 'ses-001', 'dwi'))
+    
+    shutil.move(opj(dwi_data_path, subject, 'dwi', subject+'_dwi.bvec'), 
+                opj(data_path, 'raw', 'bids', subject, 'ses-001', 'dwi'))
+    
+    shutil.move(opj(dwi_data_path, subject, 'dwi', subject+'_dwi.nii.gz'), 
+                opj(data_path, 'raw', 'bids', subject, 'ses-001', 'dwi'))
