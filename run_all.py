919d3571ec9eaab055d2bbc015cadc287f5e4aad
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-09 14:56:03 +0200

    script for slurm

--- run_all.py
+++ run_all.py
@@ -21,19 +21,17 @@ for file in os.listdir("./data"):
     filey.writelines("#SBATCH --error=.out/%s.err\n" % subject)
     filey.writelines("#SBATCH -p large\n")
     filey.writelines("#SBATCH -n 4\n")
-    filey.writelines("#SBATCH --mem=10000\n")
+    filey.writelines("#SBATCH --mem=5000\n")
     filey.writelines("\n")
 
-    filey.writelines("module load FreeSurfer/6.0.0-centos6_x86_64\n")
-    filey.writelines("source $FREESURFER_HOME/SetUpFreeSurfer.sh\n")
-    filey.writelines("export SUBJECTS_DIR=/home/aerramuzpe/surface-kljajevic-17/processed_data\n")
+    #filey.writelines("module load FreeSurfer/6.0.0-centos6_x86_64\n")
     filey.writelines("recon-all -subjid %s \
                      -i /home/aerramuzpe/surface-kljajevic-17/data/%s \
                      -all \
                      -openmp 4 \
                      -qcache \
                      \n" % (subject, file))
-    filey.writelines("module unload FreeSurfer/6.0.0-centos6_x86_64\n")
+    #filey.writelines("module unload FreeSurfer/6.0.0-centos6_x86_64\n")
 
     filey.close()
     os.system("sbatch  " + ".job/%s.job" % subject)
