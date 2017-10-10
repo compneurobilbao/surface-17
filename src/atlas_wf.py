0a843f67b81b19ae6b6607c0ed4a6915f37e8aeb
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-10 10:56:39 +0200

    clean workflow

--- src/atlas_wf.py
+++ src/atlas_wf.py
@@ -50,37 +50,3 @@ run_dtk_tractography(SUBJECT_LIST, SESSION_LIST)
 
 get_con_matrix_matlab(SUBJECT_LIST, SESSION_LIST)
 
-# Visualization
-# http://web4.cs.ucl.ac.uk/research/medic/camino/pmwiki/pmwiki.php?n=Tutorials.TrackingTutorial
-
-
-"""
-fMRI pipeline postproc
-"""
-
-clean_and_get_time_series(SUBJECT_LIST, SESSION_LIST)
-
-"""
-Electrodes location pipeline (WARNING: Some manual work)
-"""
-
-# FIRST: T1w_electrodes to 09c space
-
-t1w_electrodes_to_09c(SUBJECT_LIST)
-
-# WARNING! Create elec file for each subject manually !!
-# from src.postproc.utils import contacts_from_electrode
-# elec_name = 'OIM1'
-# contact_num = 12
-# first_contact_pos = [78, 73, 78]
-# last_contact_pos = [80, 26, 84]
-# contacts_from_electrode(first_contact_pos, last_contact_pos, contact_num, elec_name)
-
-"""
-APPROACH 1: ATLAS
-"""
-locate_electrodes(SUBJECT_LIST)
-
-# or
-
-#locate_electrodes_closest_roi(SUBJECT_LIST)
