0da252dbf37695708459fb39e269c3dc75de3579
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-17 11:03:30 +0200

    checking uploads to cluster

--- create_bids.py
+++ create_bids.py
@@ -60,3 +60,39 @@ for subject in os.listdir(fmri_data_path):
     except Exception as e:
         print(e)
         pass
+
+
+"""
+Correct uploading checking
+"""
+import os
+from os.path import join as opj
+import shutil
+cwd = '/home/asier/Desktop/CamCan'
+
+# LOCAL 
+anat_data_path = opj(cwd, 'anat')
+print(len(os.listdir(anat_data_path)))
+
+dwi_data_path = opj(cwd, 'dwi')
+print(len(os.listdir(dwi_data_path)))
+
+fmri_data_path = opj(cwd, 'func')
+print(len(os.listdir(fmri_data_path)))
+
+
+# Cluster
+import os
+from os.path import join as opj
+from bids.grabbids import BIDSLayout
+
+cwd = os.getcwd()
+bids_path = opj(cwd, 'data', 'raw', 'bids')
+
+layout = BIDSLayout(bids_path)
+files = layout.get(target='type')
+len([f.type for f in files if f.type == 'dwi'])/3 #648 OK
+len([f.type for f in files if f.type == 'T1w']) #174 NOT OK
+len([f.type for f in files if f.type == 'bold'])/2 #358 NOT OK
+
+
