eb46aedc7e99ae5638a2953f78b930a6f0ca978c
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-24 13:05:18 +0200

    modified to run in parallel

--- src/atlas_wf.py
+++ src/atlas_wf.py
@@ -7,42 +7,40 @@ from src.postproc.utils import atlas_to_t1
 from src.dmri import (run_dti_artifact_correction,
                       run_spm_fsl_dti_preprocessing,
                       run_camino_tractography,
-                      run_dtk_tractography,
                       )
 
 from src.dmri.utils import (correct_dwi_space_atlas,
-                            get_con_matrix_matlab,
                             )
 
 
-SUBJECT_LIST = ['sub-CC110037']
-SESSION_LIST = ['ses-001']
+def main_workflow(SUBJECT_LIST):
+    SESSION_LIST = ['ses-001']
 
-"""
-fmriprep and mriqc calls
-"""
+    """
+    fmriprep and mriqc calls
+    """
 
-run_fmriprep(SUBJECT_LIST, SESSION_LIST)
+    run_fmriprep(SUBJECT_LIST, SESSION_LIST)
 
-# WARNING!! Execute permission change over files before continue
-# sudo chmod 777 -R $OUTPUT_DIR 
-
-
-"""
-Atlas to T1w space
-"""
-
-atlas_to_t1(SUBJECT_LIST, SESSION_LIST)
-
-"""
-dMRI pipeline
-"""
-
-run_dti_artifact_correction(SUBJECT_LIST, SESSION_LIST)
-
-run_spm_fsl_dti_preprocessing(SUBJECT_LIST, SESSION_LIST)
-
-correct_dwi_space_atlas(SUBJECT_LIST, SESSION_LIST)
-
-run_camino_tractography(SUBJECT_LIST, SESSION_LIST)
+    # WARNING!! Execute permission change over files before continue
+    # sudo chmod 777 -R $OUTPUT_DIR 
+    
+    
+    """
+    Atlas to T1w space
+    """
+    
+    atlas_to_t1(SUBJECT_LIST, SESSION_LIST)
+    
+    """
+    dMRI pipeline
+    """
+    
+    run_dti_artifact_correction(SUBJECT_LIST, SESSION_LIST)
+    
+    run_spm_fsl_dti_preprocessing(SUBJECT_LIST, SESSION_LIST)
+    
+    correct_dwi_space_atlas(SUBJECT_LIST, SESSION_LIST)
+    
+    run_camino_tractography(SUBJECT_LIST, SESSION_LIST)
 
