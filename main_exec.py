651a05de9445dff6afe6dc73a1a1f344817e3973
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-24 13:05:32 +0200

    main_exec added

---
+++ main_exec.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 12:47:19 2017
+
+@author: asier
+"""
+from multiprocessing import Pool
+import os
+from os.path import join as opj
+from bids.grabbids import BIDSLayout
+
+from src.atlas_wf import main_workflow
+
+
+if __name__ == "__main__":
+    
+    
+    cwd = os.getcwd()
+    bids_path = opj(cwd, 'data', 'raw', 'bids')
+    layout = BIDSLayout(bids_path)
+    
+    subjects = layout.get_subjects()
+    
+    args = [tuple([sub])
+            for sub in subjects[10:11]]
+
+    pool = Pool()
+    pool.map(main_workflow, args)
+    pool.close()
