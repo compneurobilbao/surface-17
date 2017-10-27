35fbe58e4380cf7c5e98a9888a92f657a35f8c0d
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-27 06:48:10 +0200

    corrections

--- main_exec.py
+++ main_exec.py
@@ -6,12 +6,24 @@ Created on Tue Oct 24 12:47:19 2017
 @author: asier
 """
 from multiprocessing import Pool
+from multiprocessing import Process, JoinableQueue
+
 import os
 from os.path import join as opj
 from bids.grabbids import BIDSLayout
 
 from src.atlas_wf import main_workflow
 
+
+# the function for each process in our pool
+def pool_func(q):
+    while True:
+        allRenderArg, otherArg = q.get() # blocks until the queue has an item
+        try:
+            render(allRenderArg, otherArg)
+        finally: q.task_done()
+        
+        
 
 if __name__ == "__main__":
     
@@ -26,6 +38,29 @@ if __name__ == "__main__":
     args = [tuple([sub])
             for sub in subjects]
 
+
+
+
+
+# best practice to go through main for multiprocessing
+    # create the pool
+    pool_size = 8
+    pool = []
+    q = JoinableQueue()
+    for x in range(pool_size):
+        pool.append(Process(target=main_exec, args))
+
+    # start the pool, making it "daemonic" (the pool should exit when this proc exits)
+    for p in pool:
+        p.daemon = True
+        p.start()
+
+    # submit jobs to the queue
+    for i in range(totalInstances):
+        q.put((allRenderArgs[i], args[2]))
+
+    # wait for all tasks to complete, then exit
+    q.join()
     pool = Pool()
     pool.map(main_workflow, args)
     pool.close()
