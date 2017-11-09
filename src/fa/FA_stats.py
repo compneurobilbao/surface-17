fc633e133c5e61b67d3cb234b8d2f276c147474a
erramuzpe <asier.erramuzpe@gmail.com>
2017-11-09 12:47:23 +0100

    added subprocess call

--- src/fa/FA_stats.py
+++ src/fa/FA_stats.py
@@ -1,10 +1,29 @@
 #! /usr/bin/env python
 import os
 import sys
+import subprocess
+
+
+def execute(cmd):
+    popen = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
 
 
 def get_measure(file, mask, measure):
 
+    
+    command = [
+            ]
+    
+    result = subprocess.check_output(command,
+                                      shell=True)
 
     return result
 
